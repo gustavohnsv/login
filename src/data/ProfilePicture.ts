929995d1bcc01b4519a0eedc1be126413e25adbe
gustavohnsv <guvi10vieira@gmail.com>
2024-05-19 13:10:52 -0300

    feat: added static profile picture list

---
+++ src/data/ProfilePicture.ts
@@ -0,0 +1,50 @@
+export const ProfilePicture = [
+    {
+        key: 0,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/13.jpg'
+    },
+    {
+        key: 1,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/51.jpg'
+    },
+    {
+        key: 2,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/50.jpg'
+    },
+    {
+        key: 3,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/49.jpg'
+    },
+    {
+        key: 4,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/48.jpg'
+    },
+    {
+        key: 5,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/47.jpg'
+    },
+    {
+        key: 6,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/46.jpg'
+    },
+    {
+        key: 7,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/45.jpg'
+    },
+    {
+        key: 8,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/42.jpg'
+    },
+    {
+        key: 9,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/41.jpg'
+    },
+    {
+        key: 10,
+        url: 'https://mighty.tools/mockmind-api/content/abstract/38.jpg'
+    },
+    {
+        key: Number.MAX_SAFE_INTEGER,
+        url: 'https://mighty.tools/mockmind-api/content/robot/12.jpg'
+    }
+]
